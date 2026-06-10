2ac224b220b7833a54c7612960dfb4db5e64a27e
DanOrm94 <142906041+DanOrm94@users.noreply.github.com>
2026-06-10 10:05:05 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,7 +6,7 @@ export const Route = createRootRoute({
     meta: [
       { charSet: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { title: 'D&D Digital Solutions — Websites for Local Small Businesses' },
+      { title: 'DD Web Solutions — Websites for Local Small Businesses' },
       {
         name: 'description',
         content:
